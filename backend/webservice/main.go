415b27cf18b5b18e6820bfea98b04d41ed25dfdb
kasinphatspam <kasinphat@icloud.com>
2024-11-30 15:23:02 +0700

    feat: backend health check

--- backend/webservice/main.go
+++ backend/webservice/main.go
@@ -4,5 +4,10 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
 	app.Listen(":5000")
 }
